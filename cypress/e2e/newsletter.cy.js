04c42227c88ad7f210c0e68bf4e8228608212207
elvisonob <elvisonob@yahoo.com>
2023-11-09 04:52:10 +0000

    interceptor

---
+++ cypress/e2e/newsletter.cy.js
@@ -0,0 +1,13 @@
+describe('Newsletter', () => {
+  beforeEach(() => {
+    cy.task('seedDatabase');
+  });
+  it('should display a success message', () => {
+    cy.intercept('POST', '/newsletter*', { status: 201 }).as('subscribe');
+    cy.visit('/');
+    cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait('@subscribe');
+    cy.contains('Thanks for signing up');
+  });
+});
