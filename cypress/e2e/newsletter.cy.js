4f7ef12271c7940ca7be336ac914d84dea435b07
elvisonob <elvisonob@yahoo.com>
2023-11-13 04:42:10 +0000

    dealing with Network requests

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -10,4 +10,24 @@ describe('Newsletter', () => {
     cy.wait('@subscribe');
     cy.contains('Thanks for signing up');
   });
+  it('should display validation errors', () => {
+    cy.intercept('POST', '/newsletter*', {
+      message: 'Email exists already.',
+    }).as('subscribe');
+    cy.visit('/');
+    cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait('@subscribe');
+    cy.contains('Email exists already');
+  });
+  it('should successfully create a new contact', () => {
+    cy.request({
+      method: 'POST',
+      url: '/newsletter',
+      body: { email: 'test@example.com' },
+      form: true,
+    }).then((res) => {
+      expect(res.status).to.eq(201);
+    });
+  });
 });
